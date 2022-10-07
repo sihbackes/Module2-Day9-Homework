a9d4a157d14cd64b294b02411725da00563be23d
Simone <simonebackesv@gmail.com>
2022-10-07 15:44:20 +0100

    exercises 1-5

--- Functions.js
+++ Functions.js
@@ -11,7 +11,6 @@ ASSIGNMENT RULES
 /* EXERCISE 1
  Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
 */
-
 console.log(
   "\n------------------------------------- EXERCISE 1 --------------------------------"
 );
@@ -25,61 +24,120 @@ console.log(area(5, 10));
  Write a function called "crazySum" which receives two integers as parameters.
  It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
 */
-
 console.log(
   "\n------------------------------------- EXERCISE 2 --------------------------------"
 );
+
+function crazySum(a, b) {
+  if (a === b) {
+    let sum = a + b;
+    return sum * 3;
+  } else {
+    return a + b;
+  }
+}
+
+console.log(crazySum(5, 5));
+console.log(crazySum(5, 10));
 
 /* EXERCISE 3
  Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
  It should return triple their absolute difference if the given number is greater than 19.
 */
+console.log(
+  "\n------------------------------------- EXERCISE 3 --------------------------------"
+);
 
-/* WRITE YOUR ANSWER HERE */
+function crazyDiff(a) {
+  if (a > 19) {
+    let subtraction = a - 19;
+    return subtraction * 3;
+  } else {
+    return 19 - a;
+  }
+}
+console.log(crazyDiff(10));
+console.log(crazyDiff(30));
 
 /* EXERCISE 4
  Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\n------------------------------------- EXERCISE 4 --------------------------------"
+);
+
+function boundary(n) {
+  if ((n >= 20 && n <= 100) || n === 400) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(boundary(10));
+console.log(boundary(20));
+console.log(boundary(85));
+console.log(boundary(150));
+console.log(boundary(400));
 
 /* EXERCISE 5
  Write a function called "strivify" which accepts a string as a parameter.
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\n------------------------------------- EXERCISE 5 --------------------------------"
+);
 
+function strivify(string) {
+  if (string === "Strive") {
+    return string;
+  } else if (string !== "Strive") {
+    return "Strive" + " " + string;
+  }
+}
+
+console.log(strivify("Strive"));
+console.log(strivify("Simone"));
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
 */
-
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\n------------------------------------- EXERCISE 6 --------------------------------"
+);
 
 /* EXERCISE 7
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
-
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\n------------------------------------- EXERCISE 7 --------------------------------"
+);
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\n------------------------------------- EXERCISE 8 --------------------------------"
+);
 
 /* EXERCISE 9
  Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\n------------------------------------- EXERCISE 9 --------------------------------"
+);
 
 /* EXERCISE 10
  Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log(
+  "\n------------------------------------- EXERCISE 10 --------------------------------"
+);
 
 /* WHEN YOU ARE FINISHED
  Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
